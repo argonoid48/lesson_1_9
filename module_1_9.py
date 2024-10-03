a618deaf55b70745512d9cc03e6dbd088b25ba43
argonoid48 <158807187+argonoid48@users.noreply.github.com>
2024-10-03 23:31:32 +0300

    Add files via upload

---
+++ module_1_9.py
@@ -0,0 +1,9 @@
+# Практическое задание по уроку "Строки и индексация строк"
+# Андрей Галкин
+
+example = 'Трансформатор'
+print(example[0])
+print(example[-1])
+print(example[6:])
+print(example[::-1])
+print(example[1::2])
